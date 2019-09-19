3a1425bec58949ae128f3ed036632b330d33914d
Ildar Musin <zildermann@gmail.com>
2019-09-19 11:17:27 +0200

    Add basic regression tests

--- Makefile
+++ Makefile
@@ -7,6 +7,8 @@ SHLIB_LINK = -llz4
 EXTENSION = pg_cryogen
 DATA = pg_cryogen--0.1.sql
 
+REGRESS = pg_cryogen
+
 EXTRA_CLEAN = sql/pg_cryogen.sql expected/pg_cryogen.out $(REGRESSION_DATA)
 
 PG_CONFIG ?= pg_config
---
+++ sql/pg_cryogen.sql
@@ -0,0 +1,25 @@
+CREATE EXTENSION pg_cryogen;
+
+CREATE TEMPORARY TABLE tmp_data
+AS SELECT gs as id, md5(gs::TEXT) as msg FROM generate_series(1, 500) as gs;
+
+COPY tmp_data TO '/tmp/pg_cryogen_data.csv' WITH csv;
+
+CREATE TABLE data (id INT4, msg TEXT);
+COPY data FROM '/tmp/pg_cryogen_data.csv' WITH csv;
+
+SELECT * FROM data LIMIT 10;
+SELECT avg(id) FROM data;
+SELECT count(*) FROM data;
+
+CREATE INDEX data_id_idx ON data USING btree (id);
+SET enable_seqscan = f;
+EXPLAIN (COSTS OFF) SELECT * FROM data WHERE id = 500;
+SELECT * FROM data WHERE id = 500;
+DROP INDEX data_id_idx;
+CREATE INDEX data_id_idx ON data USING brin (id);
+EXPLAIN (COSTS OFF) SELECT * FROM data WHERE id = 500;
+SELECT * FROM data WHERE id = 500;
+
+COPY data FROM '/tmp/pg_cryogen_data.csv' WITH csv;
+SELECT count(*) FROM data;
