70fbd575b3957bb0d9e893d4d72561a7f948faa0
Ildar Musin <zildermann@gmail.com>
2020-04-30 11:58:20 +0200

    Fix compilation issues, patch by Pavel Stehule (@okbob)

--- compression.h
+++ compression.h
@@ -10,17 +10,17 @@ typedef enum
     COMP_ZSTD
 } CompressionMethod;
 
-int compression_method_guc;
-int lz4_acceleration_guc;
-int zstd_compression_level_guc;
+extern int compression_method_guc;
+extern int lz4_acceleration_guc;
+extern int zstd_compression_level_guc;
 
-char *cryo_compress(CompressionMethod method,
-                    const char *data,
-                    Size *compressed_size);
-bool cryo_decompress(CompressionMethod method,
-                     const char *compressed,
-                     Size compressed_size,
-                     char *out);
-void cryo_define_compression_gucs(void);
+extern char *cryo_compress(CompressionMethod method,
+                           const char *data,
+                           Size *compressed_size);
+extern bool cryo_decompress(CompressionMethod method,
+                            const char *compressed,
+                            Size compressed_size,
+                            char *out);
+extern void cryo_define_compression_gucs(void);
 
 #endif /* __COMPRESSION_H__ */
